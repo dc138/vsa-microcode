71edbeb031a9381e2d9d9884c5b5e610daab671d
DarthChungo <antonio@adaro.eu>
2022-04-09 21:37:03 +0200

    Simplified code & split microcode into separate HIGH & LOW files

--- src/microcode.cpp
+++ src/microcode.cpp
@@ -49,219 +49,137 @@
 #define CWL(bits) ((CWI ^ (bits)) & 0x00FF)
 
 uint8_t microcode[512];
+uint8_t microcode_low[256];
+uint8_t microcode_high[256];
+
+void set_addr(uint16_t addr, uint16_t bits) {
+  microcode[(addr*2)  ] = CWH(bits);
+  microcode[(addr*2)+1] = CWL(bits);
+
+  microcode_low[addr] = CWL(bits);
+  microcode_high[addr] = CWH(bits);
+}
 
 int main() {
   std::memset((void *)microcode, 0, sizeof(uint8_t) * 512);
+  std::memset((void *)microcode_low, 0, sizeof(uint8_t) * 256);
+  std::memset((void *)microcode_high, 0, sizeof(uint8_t) * 256);
   uint8_t i = 0;
 
   // clang-format off
   // Ensure invalid locations redirect back to a valid instruction code
 
   do {
-    microcode[i*2  ] = CWH(CLR);
-    microcode[i*2+1] = CWL(CLR);
+    set_addr(i, CLR);
   } while (i++ < 0xFF);
 
   // Common instruction fetch cycle
 
   do {
-    microcode[(0b00000000+i)*2  ] = CWH(PCO|MRI);
-    microcode[(0b00000000+i)*2+1] = CWL(PCO|MRI);
-
-    microcode[(0b00010000+i)*2  ] = CWH(RMO|IRI);
-    microcode[(0b00010000+i)*2+1] = CWL(RMO|IRI);
+    set_addr(0b00000000+i, PCO|MRI);
+    set_addr(0b00010000+i, RMO|IRI);
   } while (i++ < 0xF);
 
   // HLT Halt System Clock
 
-  microcode[(0b00100000*2)  ] = CWH(HLT);
-  microcode[(0b00100000*2)+1] = CWL(HLT);
+  set_addr(0b00100000, HLT);
 
   // LDA Load from Adress into Accumulator
 
-  microcode[(0b00100001*2)  ] = CWH(PCI);
-  microcode[(0b00100001*2)+1] = CWL(PCI);
-
-  microcode[(0b00110001*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00110001*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01000001*2)  ] = CWH(RMO|MRI);
-  microcode[(0b01000001*2)+1] = CWL(RMO|MRI);
-
-  microcode[(0b01010001*2)  ] = CWH(ACO|ACI|ALS);
-  microcode[(0b01010001*2)+1] = CWL(ACO|ACI|ALS);
-
-  microcode[(0b01100001*2)  ] = CWH(RMO|ACI|PCI);
-  microcode[(0b01100001*2)+1] = CWL(RMO|ACI|PCI);
+  set_addr(0b00100001, PCI);
+  set_addr(0b00110001, PCO|MRI);
+  set_addr(0b01000001, RMO|MRI);
+  set_addr(0b01010001, ACO|ACI|ALS);
+  set_addr(0b01100001, RMO|ACI|PCI);
 
   // STA Store from Accumulator into Address
 
-  microcode[(0b00100010*2)  ] = CWH(PCI);
-  microcode[(0b00100010*2)+1] = CWL(PCI);
-
-  microcode[(0b00110010*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00110010*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01000010*2)  ] = CWH(RMO|MRI);
-  microcode[(0b01000010*2)+1] = CWL(RMO|MRI);
-
-  microcode[(0b01010010*2)  ] = CWH(ACO|RMI|PCI);
-  microcode[(0b01010010*2)+1] = CWL(ACO|RMI|PCI);
-
+  set_addr(0b00100010, PCI);
+  set_addr(0b00110010, PCO|MRI);
+  set_addr(0b01000010, RMO|MRI);
+  set_addr(0b01010010, ACO|RMI|PCI);
+  
   // ADA Add to Accumulator from Address
 
-  microcode[(0b00100011*2)  ] = CWH(PCI);
-  microcode[(0b00100011*2)+1] = CWL(PCI);
-
-  microcode[(0b00110011*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00110011*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01000011*2)  ] = CWH(RMO|MRI);
-  microcode[(0b01000011*2)+1] = CWL(RMO|MRI);
-
-  microcode[(0b01010011*2)  ] = CWH(RMO|ACI|FRI|PCI);
-  microcode[(0b01010011*2)+1] = CWL(RMO|ACI|FRI|PCI);
+  set_addr(0b00100011, PCI);
+  set_addr(0b00110011, PCO|MRI);
+  set_addr(0b01000011, RMO|MRI);
+  set_addr(0b01010011, RMO|ACI|FRI|PCI);
 
   // SBA Subtract to Accumulator from Address
 
-  microcode[(0b00100100*2)  ] = CWH(PCI);
-  microcode[(0b00100100*2)+1] = CWL(PCI);
-
-  microcode[(0b00110100*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00110100*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01000100*2)  ] = CWH(RMO|MRI);
-  microcode[(0b01000100*2)+1] = CWL(RMO|MRI);
-
-  microcode[(0b01010100*2)  ] = CWH(RMO|ACI|ALS|FRI|PCI);
-  microcode[(0b01010100*2)+1] = CWL(RMO|ACI|ALS|FRI|PCI);
+  set_addr(0b00100100, PCI);
+  set_addr(0b00110100, PCO|MRI);
+  set_addr(0b01000100, RMO|MRI);
+  set_addr(0b01010100, RMO|ACI|ALS|FRI|PCI);
 
   // JPO Jump to Address if Overflow
 
-  microcode[(0b00100101*2)  ] = CWH(PCI);
-  microcode[(0b00100101*2)+1] = CWL(PCI);
-
-  microcode[(0b00110101*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00110101*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01000101*2)  ] = CWH(PCI);
-  microcode[(0b01000101*2)+1] = CWL(PCI);
-
-  microcode[(0b01010101*2)  ] = CWH(RMO|PCF);
-  microcode[(0b01010101*2)+1] = CWL(RMO|PCF);
+  set_addr(0b00100101, PCI);
+  set_addr(0b00110101, PCO|MRI);
+  set_addr(0b01000101, PCI);
+  set_addr(0b01010101, RMO|PCF);
 
   // JPZ Jump to Address if Zero
 
-  microcode[(0b00100110*2)  ] = CWH(PCI);
-  microcode[(0b00100110*2)+1] = CWL(PCI);
-
-  microcode[(0b00110110*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00110110*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01000110*2)  ] = CWH(PCI);
-  microcode[(0b01000110*2)+1] = CWL(PCI);
-
-  microcode[(0b01010110*2)  ] = CWH(RMO|PCZ);
-  microcode[(0b01010110*2)+1] = CWL(RMO|PCZ);
+  set_addr(0b00100110, PCI);
+  set_addr(0b00110110, PCO|MRI);
+  set_addr(0b01000110, PCI);
+  set_addr(0b01010110, RMO|PCZ);
 
   // OTA Output Memory at Address
 
-  microcode[(0b00100111*2)  ] = CWH(PCI);
-  microcode[(0b00100111*2)+1] = CWL(PCI);
-
-  microcode[(0b00110111*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00110111*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01000111*2)  ] = CWH(RMO|MRI);
-  microcode[(0b01000111*2)+1] = CWL(RMO|MRI);
-
-  microcode[(0b01010111*2)  ] = CWH(RMO|OTI|PCI);
-  microcode[(0b01010111*2)+1] = CWL(RMO|OTI|PCI);
+  set_addr(0b00100111, PCI);
+  set_addr(0b00110111, PCO|MRI);
+  set_addr(0b01000111, RMO|MRI);
+  set_addr(0b01010111, RMO|OTI|PCI);
 
   // JMP Jump to Address
 
-  microcode[(0b00101000*2)  ] = CWH(PCI);
-  microcode[(0b00101000*2)+1] = CWL(PCI);
-
-  microcode[(0b00111000*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00111000*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01001000*2)  ] = CWH(RMO|PCJ);
-  microcode[(0b01001000*2)+1] = CWL(RMO|PCJ);
+  set_addr(0b00101000, PCI);
+  set_addr(0b00111000, PCO|MRI);
+  set_addr(0b01001000, RMO|PCJ);
 
   // LDI Load from Imidiate into Accumulator
 
-  microcode[(0b00101001*2)  ] = CWH(PCI);
-  microcode[(0b00101001*2)+1] = CWL(PCI);
-
-  microcode[(0b00111001*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00111001*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01001001*2)  ] = CWH(ACO|ACI|ALS);
-  microcode[(0b01001001*2)+1] = CWL(ACO|ACI|ALS);
-
-  microcode[(0b01011001*2)  ] = CWH(RMO|ACI|PCI);
-  microcode[(0b01011001*2)+1] = CWL(RMO|ACI|PCI);
+  set_addr(0b00101001, PCI);
+  set_addr(0b00111001, PCO|MRI);
+  set_addr(0b01001001, ACO|ACI|ALS);
+  set_addr(0b01011001, RMO|ACI|PCI);
 
   // NOP No Operation
 
-  microcode[(0b00101010*2)  ] = CWH(PCI);
-  microcode[(0b00101010*2)+1] = CWL(PCI);
+  set_addr(0b00101010, PCI);
 
   // ADI Add to Accumulator from Imidiate
 
-  microcode[(0b00101011*2)  ] = CWH(PCI);
-  microcode[(0b00101011*2)+1] = CWL(PCI);
-
-  microcode[(0b00111011*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00111011*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01001011*2)  ] = CWH(RMO|ACI|FRI|PCI);
-  microcode[(0b01001011*2)+1] = CWL(RMO|ACI|FRI|PCI);
+  set_addr(0b00101011, PCI);
+  set_addr(0b00111011, PCO|MRI);
+  set_addr(0b01001011, RMO|ACI|FRI|PCI);
 
   // SBI Subtract to Accumulator from Imidiate
 
-  microcode[(0b00101100*2)  ] = CWH(PCI);
-  microcode[(0b00101100*2)+1] = CWL(PCI);
-
-  microcode[(0b00111100*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00111100*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01001100*2)  ] = CWH(RMO|ACI|ALS|FRI|PCI);
-  microcode[(0b01001100*2)+1] = CWL(RMO|ACI|ALS|FRI|PCI);
+  set_addr(0b00101100, PCI);
+  set_addr(0b00111100, PCO|MRI);
+  set_addr(0b01001100, RMO|ACI|ALS|FRI|PCI);
 
   // JNO Jump to Address if not Overflow
 
-  microcode[(0b00101101*2)  ] = CWH(PCI);
-  microcode[(0b00101101*2)+1] = CWL(PCI);
-
-  microcode[(0b00111101*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00111101*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01001101*2)  ] = CWH(PCI);
-  microcode[(0b01001101*2)+1] = CWL(PCI);
-
-  microcode[(0b01011101*2)  ] = CWH(RMO|PCF|PCJ);
-  microcode[(0b01011101*2)+1] = CWL(RMO|PCF|PCJ);
+  set_addr(0b00101101, PCI);
+  set_addr(0b00111101, PCO|MRI);
+  set_addr(0b01001101, PCI);
+  set_addr(0b01011101, RMO|PCF|PCJ);
 
   // JNZ Jump to Address if not Zero
 
-  microcode[(0b00101110*2)  ] = CWH(PCI);
-  microcode[(0b00101110*2)+1] = CWL(PCI);
-
-  microcode[(0b00111110*2)  ] = CWH(PCO|MRI);
-  microcode[(0b00111110*2)+1] = CWL(PCO|MRI);
-
-  microcode[(0b01001110*2)  ] = CWH(PCI);
-  microcode[(0b01001110*2)+1] = CWL(PCI);
-
-  microcode[(0b01011110*2)  ] = CWH(RMO|PCZ|PCJ);
-  microcode[(0b01011110*2)+1] = CWL(RMO|PCZ|PCJ);
+  set_addr(0b00101110, PCI);
+  set_addr(0b00111110, PCO|MRI);
+  set_addr(0b01001110, PCI);
+  set_addr(0b01011110, RMO|PCZ|PCJ);
 
   // OTC Output Accumulator
 
-  microcode[(0b00101111*2)  ] = CWH(ACO|OTI|PCI);
-  microcode[(0b00101111*2)+1] = CWL(ACO|OTI|PCI);
+  set_addr(0b00101111, ACO|OTI|PCI);
 
   // clang-format on
 
@@ -269,6 +187,16 @@ int main() {
   out.open("microcode.rom", std::ofstream::binary);
   out.write((char *)microcode, sizeof(uint8_t) * 512);
   out.close();
+
+  std::ofstream out_low;
+  out_low.open("microcode_low.rom", std::ofstream::binary);
+  out_low.write((char *)microcode_low, sizeof(uint8_t) * 256);
+  out_low.close();
+
+  std::ofstream out_high;
+  out_high.open("microcode_high.rom", std::ofstream::binary);
+  out_high.write((char *)microcode_high, sizeof(uint8_t) * 256);
+  out_high.close();
 
   return 0;
 }
